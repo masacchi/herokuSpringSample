fa777aeea68a8042763cc31f93fd2542f6f3c1e1
masato <mstaa0_8_4b@ezweb.ne.jp>
2017-07-17 10:52:54 +0900

    first commit!

---
+++ src/main/java/jp/masato/spring/sample/HerokusampleApplication.java
@@ -0,0 +1,12 @@
+package jp.masato.spring.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerokusampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerokusampleApplication.class, args);
+	}
+}
